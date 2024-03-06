8408c8efe981e509c8baa8489e3bc48e4185bf1d
laurenpowers20 <lauren.powers86@yahoo.com>
2024-03-06 11:18:39 -0500

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="">
+      <h1>About</h1>
+      <p>Here's about me. </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/app/travel/page.jsx
@@ -0,0 +1,5 @@
+const Travel = () => {
+  return <div className="">Travel</div>;
+};
+
+export default Travel;
