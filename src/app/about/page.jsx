7fdb9db50e646826a4aa80bb4319323b6c376136
laurenpowers20 <lauren.powers86@yahoo.com>
2024-03-17 20:35:37 -0400

    about me section start

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,6 +3,7 @@
 import { motion, useInView } from "framer-motion";
 import Image from "next/image";
 import { useRef } from "react";
+import DownArrow from "@/components/down";
 
 const About = () => {
   const containerRef = useRef();
@@ -23,15 +24,21 @@ const About = () => {
       {/* CONTAINER */}
       <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
         {/* TEXT CONTAINER */}
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-14 md:gap-22 lg:gap-38 xl:gap-54 lg:w-2/3 lg:pr-0 xl:w-1/2">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2 ">
           {/* BIOGRAPHY CONTAINER */}
           <div className="flex flex-col gap-5 justify-center">
             {/* BIOGRAPHY IMAGE */}
-
+            <Image
+              src="/hero.png"
+              alt=""
+              width={200}
+              height={200}
+              className="w-50 h-50 object-cover"
+            />
             {/* BIOGRAPHY TITLE */}
-            <h1 className="font-bold text-2xl">ABOUT ME</h1>
+            <h1 className="font-rakkasReg text-3xl">ABOUT ME</h1>
             {/* BIOGRAPHY DESC */}
-            <p className="text-lg ">
+            <p className="text-xl font-latoReg">
               Once a solo hostel backpacker, I`ve since transformed into a luxe
               adventurer. My journey has taken me from exploring the cities of
               Europe, reveling in the world`s finest art museums and vibrant
@@ -42,7 +49,7 @@ const About = () => {
 
             {/* BIOGRAPHY SCROLL SVG */}
 
-            <motion.svg
+            <motion.div
               initial={{ opacity: 0.2, y: 0 }}
               animate={{ opacity: 1, y: "10px" }}
               transition={{
@@ -52,42 +59,22 @@ const About = () => {
               }}
               viewBox="0 0 24 24"
               fill="none"
+              className="w-10 h-10 my-10"
               xmlns="http://www.w3.org/2000/svg"
               width={50}
               height={50}
             >
-              <path
-                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
-                stroke="#000000"
-                strokeWidth="1"
-              ></path>
-              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
-              <path
-                d="M15 11L12 14L9 11"
-                stroke="#000000"
-                strokeWidth="1"
-              ></path>
-            </motion.svg>
-
-            <motion.h2
-              initial={{ opacity: 0.2, y: 0 }}
-              animate={{ opacity: 1, y: "10px" }}
-              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              width={50}
-              height={50}
-            ></motion.h2>
+              <DownArrow />
+            </motion.div>
           </div>
           {/* SKILLS CONTAINER */}
           <div className="flex flex-col gap-12 justify-center" ref={skillRef}>
-            {/* SKILL TITLE */}
+            {/* {TITLE} */}
             <motion.h1
               initial={{ x: "-500px" }}
               animate={isSkillRefInView ? { x: 0 } : {}}
               transition={{ delay: 0.2 }}
-              className="font-bold text-2xl"
+              className="font-rakkasReg text-3xl"
             >
               I HAVE EXPERIENCE IN
             </motion.h1>
@@ -98,7 +85,7 @@ const About = () => {
             {/* SKILL SCROLL SVG */}
             {/* BIOGRAPHY SCROLL SVG */}
 
-            <motion.svg
+            <motion.div
               initial={{ opacity: 0.2, y: 0 }}
               animate={{ opacity: 1, y: "10px" }}
               transition={{
@@ -108,43 +95,24 @@ const About = () => {
               }}
               viewBox="0 0 24 24"
               fill="none"
+              className="w-10 h-10 "
               xmlns="http://www.w3.org/2000/svg"
               width={50}
               height={50}
             >
-              <path
-                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
-                stroke="#000000"
-                strokeWidth="1"
-              ></path>
-              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
-              <path
-                d="M15 11L12 14L9 11"
-                stroke="#000000"
-                strokeWidth="1"
-              ></path>
-            </motion.svg>
-            <motion.svg
-              initial={{ opacity: 0.2, y: 0 }}
-              animate={{ opacity: 1, y: "10px" }}
-              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              width={50}
-              height={50}
-            ></motion.svg>
+              <DownArrow />
+            </motion.div>
           </div>
           {/* EXPERIENCE CONTAINER */}
           <div
-            className="flex flex-col gap-12 justify-center pb-28"
+            className="flex flex-col gap-12 justify-center pb-38"
             ref={experienceRef}
           >
             <motion.h1
               initial={{ x: "-100px" }}
               animate={isExperienceRefInView ? { x: "0" } : {}}
               transition={{ delay: 0.2 }}
-              className="font-bold text-2xl"
+              className="font-rakkasReg text-3xl"
             >
               FUN FACTS
             </motion.h1>
@@ -155,12 +123,7 @@ const About = () => {
               className=""
             >
               <ul>
-                <motion.li
-                  initial={{ x: "-300px" }}
-                  animate={isExperienceRefInView ? { x: "0" } : {}}
-                >
-                  Bullet POInt 1
-                </motion.li>
+                <motion.li>Bullet POInt 1</motion.li>
                 <motion.li
                   initial={{ x: "-300px" }}
                   animate={isExperienceRefInView ? { x: "0" } : {}}
@@ -184,6 +147,9 @@ const About = () => {
           </div>
         </div>
         {/* Picture*/}
+        <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2 justify-center">
+          {/* <Image src="/hero.png" alt="" fill className="object-contain" /> */}
+        </div>
       </div>
     </motion.div>
   );
