86a396dfdcbe2c8dcb1de95a75181ec7ad1ca4aa
laurenpowers20 <lauren.powers86@yahoo.com>
2024-03-26 15:24:34 -0400

    services table

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -16,9 +16,65 @@ const Services = () => {
             Services
           </h1>
           <div>
-            <h2 className="text-2xl md:text-3xl  justify-center pb-5">
-              Destination Research
-            </h2>
+            <table class="table-auto">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="p-5">
+                    {" "}
+                    <h2 className="pb-5 text-2xl md:text-3xl  justify-center ">
+                      Destination Research
+                    </h2>
+                    <p className=" font-ovo text-1xl md:text-2xl pb-5">
+                      If you know where you want to go and just need help
+                      booking hotels, resorts, villas, and cruises{" "}
+                    </p>
+                  </td>
+                  <td>
+                    <p className="font-ovo text-1xl md:text-2xl">
+                      Starting at $0*{" "}
+                      <p className="text-sm">
+                        Free when booking qualifying hotels, resorts, villas,
+                        and cruises
+                      </p>
+                    </p>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-5">
+                    {" "}
+                    <h2 className=" text-2xl md:text-3xl  justify-center ">
+                      Activites & More
+                    </h2>
+                    <p className=" font-ovo text-1xl md:text-2xl">
+                      You booked your flight and hotel, but want some
+                      recommendations on activities and restaurant
+                    </p>
+                  </td>
+                  <td className="font-ovo text-1xl md:text-2xl">
+                    Starting at $75
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-5">
+                    <h2 className=" text-2xl md:text-3xl  justify-center ">
+                      Full Service Hotel & Activities Itinerary
+                    </h2>
+                    <p className=" font-ovo text-1xl md:text-2xl">
+                      Book accommodation, activities, airport transportation
+                    </p>
+                  </td>
+                  <td className="font-ovo text-1xl md:text-2xl">
+                    Starting at $100
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </motion.div>
       </div>
