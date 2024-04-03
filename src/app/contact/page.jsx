aabc84aa8497a3a08fc933803341258c2aa0558f
laurenpowers20 <lauren.powers86@yahoo.com>
2024-04-02 20:32:19 -0400

    add redirects

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import "../../components/carousel/style.scss";
-// import Form from "@/components/form/form";
+
 import Link from "next/link";
 import Image from "next/image";
 
